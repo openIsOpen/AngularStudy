e1901d749b903862981f15a56b419bdd433f679c
zhangliwen <liwen9016@126.com>
2019-11-04 17:21:43 +0800

    Reactive form half

---
+++ FormPractice/src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl} from '@angular/forms'
+
+@Component({
+  selector: 'app-profile-editor',
+  templateUrl: './profile-editor.component.html',
+  styleUrls: ['./profile-editor.component.css']
+})
+export class ProfileEditorComponent implements OnInit {
+
+  profileForm = new FormGroup({
+    firstName:new FormControl(''),
+    lastName : new FormControl(''),
+    address:new FormGroup({
+      street:new FormControl(''),
+      city : new FormControl(''),
+      state: new FormControl(''),
+      zip  : new FormControl('')
+    })
+  });
+
+  onSubmit(){
+    console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
